7f1a6694369c167ff8b2963217bf6adac21f45bd
Kris-fynn <67267786+Kris-fynn@users.noreply.github.com>
2022-12-19 00:59:59 +0200

    do while added
    do while by kris

---
+++ DoWhileLoops/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim iCount As Integer
+
+        iCount = 0
+        Do While iCount < 5
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop
+
+        iCount = 0
+        Do
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop While iCount < 5
+
+        iCount = 0
+        Do Until iCount = 5
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop
+
+        iCount = 0
+        Do
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop Until iCount = 5
+
+
+        MsgBox("all done")
+
+    End Sub
+End Class
