31e75ced6d4be0996d1d0b51d297ab762950c060
Kris-fynn <67267786+Kris-fynn@users.noreply.github.com>
2022-12-19 23:06:49 +0200

    Do loop Validation added By Kris
    Do loop Validation

---
+++ DoLoopzValidation/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        Dim stName As String
+
+        Do While stName <> "Kris"
+            stName = InputBox("Please enter your name")
+        Loop
+
+        MsgBox("Hello " & stName)
+    End Sub
+End Class
