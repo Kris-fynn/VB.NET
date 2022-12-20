da287cabf9bf0eb5adbcda63dd62ccea5abf8b7d
Kris-fynn <67267786+Kris-fynn@users.noreply.github.com>
2022-12-20 02:40:05 +0200

    updates
    updates

---
+++ Two Dimensional Arrays/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematcian"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Eistein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Ghandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = " van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        MsgBox(astPeople(4, 3))
+
+
+
+
+    End Sub
+End Class
