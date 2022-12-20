a44691472659141c02c1f3adc596ea2a0e572cd5
Kris-fynn <67267786+Kris-fynn@users.noreply.github.com>
2022-12-21 01:25:41 +0200

    Added 2D Arrays and Nested loops
    Added 2D Arrays and Nested loops by Kris

---
+++ Two Dimesion Arrays and Nested Loops/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRowWise = New System.Windows.Forms.Button()
+        Me.btnColumnWise = New System.Windows.Forms.Button()
+        Me.btnOutputAll = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnRowWise
+        '
+        Me.btnRowWise.Location = New System.Drawing.Point(83, 53)
+        Me.btnRowWise.Name = "btnRowWise"
+        Me.btnRowWise.Size = New System.Drawing.Size(194, 50)
+        Me.btnRowWise.TabIndex = 0
+        Me.btnRowWise.Text = "Display data terms row wise"
+        Me.btnRowWise.UseVisualStyleBackColor = True
+        '
+        'btnColumnWise
+        '
+        Me.btnColumnWise.Location = New System.Drawing.Point(83, 133)
+        Me.btnColumnWise.Name = "btnColumnWise"
+        Me.btnColumnWise.Size = New System.Drawing.Size(194, 50)
+        Me.btnColumnWise.TabIndex = 1
+        Me.btnColumnWise.Text = "Display data terms column wise"
+        Me.btnColumnWise.UseVisualStyleBackColor = True
+        '
+        'btnOutputAll
+        '
+        Me.btnOutputAll.Location = New System.Drawing.Point(83, 225)
+        Me.btnOutputAll.Name = "btnOutputAll"
+        Me.btnOutputAll.Size = New System.Drawing.Size(194, 50)
+        Me.btnOutputAll.TabIndex = 2
+        Me.btnOutputAll.Text = "Display all data terms  at once"
+        Me.btnOutputAll.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(83, 316)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(194, 50)
+        Me.btnSearch.TabIndex = 3
+        Me.btnSearch.Text = "Search for a person"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(363, 450)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnOutputAll)
+        Me.Controls.Add(Me.btnColumnWise)
+        Me.Controls.Add(Me.btnRowWise)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnRowWise As Button
+    Friend WithEvents btnColumnWise As Button
+    Friend WithEvents btnOutputAll As Button
+    Friend WithEvents btnSearch As Button
+End Class
---
+++ Two Dimesion Arrays and Nested Loops/Form1.vb
@@ -0,0 +1,232 @@
+﻿Public Class Form1
+    Private Sub btnRowWise_Click(sender As Object, e As EventArgs) Handles btnRowWise.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematcian"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Eistein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Ghandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = " van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+
+
+
+        For y = 0 To 5
+            For x = 0 To 4
+                MsgBox(astPeople(x, y))
+            Next
+        Next
+
+
+    End Sub
+
+    Private Sub btnColumnWise_Click(sender As Object, e As EventArgs) Handles btnColumnWise.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematcian"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Eistein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Ghandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = " van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+
+        For x = 0 To 4
+            For y = 0 To 5
+                MsgBox(astPeople(x, y))
+            Next
+        Next
+
+
+    End Sub
+
+    Private Sub btnOutputAll_Click(sender As Object, e As EventArgs) Handles btnOutputAll.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematcian"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Eistein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Ghandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = " van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim stOut As String
+
+        For y = 0 To 5
+            For x = 0 To 4
+                stOut = stOut & astPeople(x, y) & " "
+            Next
+            stOut = stOut & vbNewLine
+        Next
+
+        MsgBox(stOut)
+
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematcian"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Eistein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Ghandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = "van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim bFound As Boolean
+        Dim stTarget As String
+        Dim stOut As String
+
+        bFound = False
+
+        stTarget = InputBox("Enter the Surname of the person you want ti find")
+
+        For y = 0 To 5
+            If astPeople(1, y) = stTarget Then
+                bFound = True
+                Exit For
+            End If
+        Next
+
+        If bFound = True Then
+            For x = 0 To 4
+                stOut = stOut & astPeople(x, y) & " "
+            Next
+        Else
+            MsgBox("not found")
+        End If
+
+        MsgBox(stOut)
+
+    End Sub
+End Class
